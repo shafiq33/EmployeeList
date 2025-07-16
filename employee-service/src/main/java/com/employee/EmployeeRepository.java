7c8e238cef08df9957c35b0554d47e7275f6d4fd
Muhammad Huzaifa Shafiq <148398435+shafiq33@users.noreply.github.com>
2025-07-16 16:10:13 -0400

    Delete employee-service/src/main/java/com/employee/EmployeeRepository.java

--- employee-service/src/main/java/com/employee/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.employee.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-  
-  
-}
