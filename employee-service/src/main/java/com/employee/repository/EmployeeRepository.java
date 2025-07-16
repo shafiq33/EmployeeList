d79cfe542e299eb2785551af22d4655c749a1afd
Muhammad Huzaifa Shafiq <148398435+shafiq33@users.noreply.github.com>
2025-07-16 17:21:59 -0400

    Update EmployeeRepository.java

--- employee-service/src/main/java/com/employee/repository/EmployeeRepository.java
+++ employee-service/src/main/java/com/employee/repository/EmployeeRepository.java
@@ -3,7 +3,9 @@ package com.employee.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.employee.entity.Employee; 
+
+@Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-  
   
 }
