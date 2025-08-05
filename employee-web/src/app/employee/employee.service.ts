bc3db83fafb73a0365b41b05109aca2a003f4f74
Muhammad Huzaifa Shafiq <148398435+shafiq33@users.noreply.github.com>
2025-08-04 20:19:43 -0400

    Update employee.service.ts

--- employee-web/src/app/employee/employee.service.ts
+++ employee-web/src/app/employee/employee.service.ts
@@ -14,16 +14,19 @@ export class EmployeeService {
   baseUrl: String="/api/v1/employees";
 
   fetchAllEmployees(): Observable<Employee[]> {
-    return this._httpClient.get<Employee[]>(`${this.baseUrl}`)
+    return this._httpClient.get<Employee[]>(`${this.baseUrl}`);
   }
   
   createEmployee(data:Employee):  {
-     return this._httpClient.post<Employee[]>(`${this.baseUrl}`, data)
+     return this._httpClient.post<Employee[]>(`${this.baseUrl}`, data);
   }
 
   updateEmployee(data:Employee):  {
-     return this._httpClient.post<Employee[]>(`${this.baseUrl}/${data.id}`, data)
+     return this._httpClient.post<Employee[]>(`${this.baseUrl}/${data.id}`, data);
   }
-  
+
+   deleteEmployee(id:Number):  {
+     return this._httpClient.delete<Employee[]>(`${this.baseUrl}/${id}`);
+  }
   
 }
